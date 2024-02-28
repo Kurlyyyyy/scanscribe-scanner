0988064179bc4f7a178c24d42ea4794989e5e295
Kurlyyyyy <157552956+Kurlyyyyy@users.noreply.github.com>
2024-02-28 23:46:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-const wrapper = document.querySelector(".wrapper"),
-form = document.querySelector("form"),
-fileInp = form.querySelector("input"),
-infoText = form.querySelector("p"),
-closeBtn = document.querySelector(".close"),
-copyBtn = document.querySelector(".copy");
+// script.js file
 
-function fetchRequest(file, formData) {
-    infoText.innerText = "Scanning QR Code...";
-    fetch("http://api.qrserver.com/v1/read-qr-code/", {
-        method: 'POST', body: formData
-    }).then(res => res.json()).then(result => {
-        result = result[0].symbol[0].data;
-        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
-        if(!result) return;
-        document.querySelector("textarea").innerText = result;
-        form.querySelector("img").src = URL.createObjectURL(file);
-        wrapper.classList.add("active");
-    }).catch(() => {
-        infoText.innerText = "Couldn't scan QR Code";
-    });
+function domReady(fn) {
+	if (
+		document.readyState === "complete" ||
+		document.readyState === "interactive"
+	) {
+		setTimeout(fn, 1000);
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
 }
 
-fileInp.addEventListener("change", async e => {
-    let file = e.target.files[0];
-    if(!file) return;
-    let formData = new FormData();
-    formData.append('file', file);
-    fetchRequest(file, formData);
-});
+domReady(function () {
 
-copyBtn.addEventListener("click", () => {
-    let text = document.querySelector("textarea").textContent;
-    navigator.clipboard.writeText(text);
-});
+	// If found you qr code
+	function onScanSuccess(decodeText, decodeResult) {
+		alert("You Qr is : " + decodeText, decodeResult);
+	}
 
-form.addEventListener("click", () => fileInp.click());
-closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
+	let htmlscanner = new Html5QrcodeScanner(
+		"my-qr-reader",
+		{ fps: 10, qrbos: 250 }
+	);
+	htmlscanner.render(onScanSuccess);
+});

9133154e35f7b05d64e27b8fa51d3b1dc6cb11ac
Kurlyyyyy <157552956+Kurlyyyyy@users.noreply.github.com>
2024-02-28 22:08:38 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ domReady(function () {
 
 	// If found you qr code
 	function onScanSuccess(decodeText, decodeResult) {
-		alert(<script type="text/javascript">
-		document.write('<a href="'+desiredLink+'">'+desiredText+'</a>');
-		</script>);
+		alert("You Qr is : " + decodeText, decodeResult);
 	}
 	let htmlscanner = new Html5QrcodeScanner(
 		"my-qr-reader",
